247ef42a4340d70757e9263e92bdc2c8a5dac3a6
Tia Krofel <tia.krofel@gmail.com>
2020-05-18 19:35:37 +0200

    Dodal bottle

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
+import random
+
 STEVILO_DOVOLJENIH_NAPAK = 10
+
+ZACETEK = 'Z'
 
 # Konstante za rezultate ugibanj
 PRAVILNA_CRKA = '+'
@@ -70,8 +74,41 @@ class Igra:
             else:
                 return NAPACNA_CRKA
 
-import random
 
 def nova_igra():
     nakljucna_beseda = random.choice(bazen_besed)
     return Igra(nakljucna_beseda)
+
+
+class Vislice:
+    """
+    Skrbi za trenutno stanje VEČ iger (imel bo več objektov tipa Igra)
+    """
+    def __init__(self,):
+        # Slovar, ki ID-ju priredi objekt njegove igre
+        self.igre = {}  # int -> (Igra, stanje)
+    
+    def prosti_id_igre(self):
+        """Vrne nek id, ki ga ne uporablja nobena igra"""
+        if len(self.igre) == 0:
+            return 0
+        else:
+            return max(self.igre.keys()) + 1
+    
+    def nova_igra(self):
+        # Dobimo svež id:
+        nov_id = self.prosti_id_igre()
+        # Naredimo novo igro:
+        sveza_igra = nova_igra()
+        # Vse to shranimo v self.igre:
+        self.igre[nov_id] = (sveza_igra, ZACETEK)
+        # Vrnemo nov id:
+        return nov_id 
+
+    def ugibaj(self, id_igre, crka):
+        # Dobimo staro igro ven:
+        trenutna_igra, _ = self.igre[id_igre]    
+        # Ugibamo crko:
+        novo_stanje = trenutna_igra.ugibaj(crka)
+        # Zapišemo posodobljeno stanje in igro nazaj v "BAZO":
+        self.igre[id_igre] = (trenutna_igra, novo_stanje)
---
+++ views/igra.tpl
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <h1>Vislice</h1>
+
+  <blockquote>
+    Vislice so najboljša igra za preganjanje dolgčasa (poleg tetrisa).
+    <small>Študentje</small>
+  </blockquote>
+
+  <h2> {{ igra.pravilni_deli_gesla() }} </h2>
+
+  <h2> Napačnih ugibov: {{ igra.nepravilni_ugibi() }} </h2>
+
+  % if poskus == "W":
+    <h1> ZMAGAL SI </h1>
+  % elif poskus == "X":
+    <h1> IZGUBIL SI </h1>
+  % else:
+  <form action="/igra/{{id_igre}}/" method="post">
+    Črka: <input type="text" name="crka">
+    <button type="submit">Ugibaj novo crko</button>
+  </form>
+  % end
+
+  <img src="img/10.jpg" alt="obesanje">
+
+  <form action="/igra/" method="post">
+    <button type="submit">Nova igra</button>
+  </form>
+
+</body>
+
+</html>
